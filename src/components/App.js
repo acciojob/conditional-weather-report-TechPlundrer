45cf662b86ae54516b6bb5539bf018f222a29650
Uttam Gawas <uttam@whizti.com>
2026-03-26 15:21:14 +0530

    2

--- src/components/App.js
+++ src/components/App.js
@@ -9,10 +9,24 @@ const App = () => {
   });
   return (
     <div>
+      
         {/* Do not remove the main div */}
         <weatherDisplay weather={weather} />
     </div>
   )
 }
+function weatherDisplay({ weather }) {
+  const {temperature, conditions } = weather;
 
+  const tempColor = temperature > 20 ? "red" : "blue";
+
+  return (
+      <div>
+          <p>
+              Temperature: <span style= {{color: tempColor}}>{temperature}</span>
+          </p>
+          <p>Conditions: {conditions}</p>
+      </div>
+  );
+}
 export default App
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,18 +1,5 @@
 import React from "react";
 
-function weatherDisplay({ weather }) {
-    const {temperature, conditions } = weather;
 
-    const tempColor = temperature > 20 ? "red" : "blue";
-
-    return (
-        <div>
-            <p>
-                Temperature: <span style= {{color: tempColor}}>{temperature}</span>
-            </p>
-            <p>Conditions: {conditions}</p>
-        </div>
-    );
-}
 
 export default weatherDisplay;
